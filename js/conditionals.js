8cd3399158b0f617f284707320d12cace26e2db5
Earl Spangenberg <espange@aol.net>
2019-06-27 13:07:47 -0400

    new commit

--- js/conditionals.js
+++ js/conditionals.js
@@ -10,3 +10,14 @@ console.log('double ==', numOne == stringOne);
 console.log('triple ===', numOne === stringOne);
 
 
+const day = new Date().getDay();
+if (day == 1) {
+    console.log("Back to work!");
+{elseif (day == 3)}
+    console.log("Over the hump!");
+{elseif (day == 6) || (day ==7)}
+    console.log("It's the weekend!");
+} else {
+    console.log("It's a workday");
+}
+
---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+function displayName(name) {
+    console.log('Hello, ' + name)
+}
+
+
+displayName("Earl");
+
